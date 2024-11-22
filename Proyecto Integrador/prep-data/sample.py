5923ba6ac0733ba945ef77f129fe3c4fd6181293
“mvasqueze” <mvasqueze@eafit.edu.co>
2024-11-22 00:46:04 -0500

    Generación de un nuevo xlsx en antioquia

--- Proyecto Integrador/prep-data/sample.py
+++ Proyecto Integrador/prep-data/sample.py
@@ -5,7 +5,7 @@ file_path = "clean_pcd_data.csv"  # Replace with your CSV file path
 data = pd.read_csv(file_path)
 
 # Randomly sample 300 rows
-sampled_data = data.sample(n=200, random_state=42)  # `random_state` ensures reproducibility
+sampled_data = data.sample(n=150, random_state=42)  # `random_state` ensures reproducibility
 
 # Save the sampled data to a new CSV file
 output_path = "sampled_file.csv"  # Replace with desired output file name
